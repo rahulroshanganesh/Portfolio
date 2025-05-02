edf5edda2e679e3100eaba721595b5e5a86f02ef
Rahul Roshan G <103802956+rahulroshanganesh@users.noreply.github.com>
2025-05-02 15:52:37 +0530

    added files

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('public'));
+
+app.post('/contact', (req, res) => {
+    const {name, email, subject, message} = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user:'rahulroshanganesh2002@gmail.com',
+            pass:'vptu paov ngon ncsn'
+        }
+    });
+
+    const mailOptions = {
+        from: email,
+        to: 'rahulroshanganesh2002@gmail.com',
+        subject:`Portflio contact - ${subject}`,
+        text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
+    };
+
+    transporter.sendMail(mailOptions, (err, info)=>{
+        if(err){
+            console.log(err);
+            res.status(500).send("Semething went wrong");
+        }else{
+            // console.log('Email sent: ' + info.response);
+            // Redirect to homepage after successful submission
+            return res.redirect('/');
+        }
+    });
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+app.listen(3000, ()=> console.log('Server running on port 3000'));
